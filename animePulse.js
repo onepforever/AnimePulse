135dbd2ad08cf7080563a649ca3c022779b56691
onepforever <130248904+onepforever@users.noreply.github.com>
2023-04-08 16:17:04 -0400

    Add side-by-side AniList and Bing News results to search page

--- animePulse.js
+++ animePulse.js
@@ -3,6 +3,7 @@
 // Replace with your actual Bing News Search API key
 const bingNewsApiKey = '8388305ec021421e9a7911e9c475133c';
 
+
 // GraphQL query for searching anime using the AniList API
 const SEARCH_ANIME_QUERY = `
   query ($search: String) {
@@ -22,8 +23,8 @@ const SEARCH_ANIME_QUERY = `
   }
 `;
 
-// Anime search functionality on the search page
-function searchAnime() {
+// Anime search and news search functionality on the search page
+function searchAnimeAndNews() {
   // Get the search query from the input field
   const searchInput = document.querySelector('.search-input');
   const query = searchInput.value.trim();
@@ -32,7 +33,10 @@ function searchAnime() {
   if (query.length > 0) {
     // AniList API URL
     const anilistApiUrl = 'https://graphql.anilist.co';
-    
+
+    // Bing News Search API URL
+    const newsUrl = `https://api.bing.microsoft.com/v7.0/news/search?q=${encodeURIComponent(query)}&count=5&apiKey=${bingNewsApiKey}`;
+
     // Fetch data from the AniList API using a POST request with the GraphQL query
     fetch(anilistApiUrl, {
       method: 'POST',
@@ -49,11 +53,11 @@ function searchAnime() {
       .then(data => {
         // Get the search results
         const results = data.data.Page.media;
-        
+
         // Clear any previous search results
-        const searchResultsDiv = document.querySelector('.search-results');
+        const searchResultsDiv = document.querySelector('.anilist-search-results');
         searchResultsDiv.innerHTML = '';
-        
+
         // Display search results
         results.forEach(anime => {
           const animeTitle = anime.title.english || anime.title.romaji;
@@ -74,54 +78,42 @@ function searchAnime() {
       .catch(error => {
         console.error('Error fetching anime search data:', error);
       });
-  }
-}
 
-// Fetch and display anime news on the news page
-function fetchAnimeNews() {
-  // Bing News Search API URL
-  const newsUrl = `https://api.bing.microsoft.com/v7.0/news/search?q=anime&count=5&apiKey=${bingNewsApiKey}`;
-  
-  // Fetch data from the Bing News Search API
-  fetch(newsUrl, {
-    headers: {
-      'Ocp-Apim-Subscription-Key': bingNewsApiKey // Include the API key in the request headers
-    }
+    // Fetch data from the Bing News Search API
+    fetch(newsUrl, {
+      headers: {
+        'Ocp-Apim-Subscription-Key': bingNewsApiKey // Include the API key in the request headers
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Get the news articles
+        const newsArticles = data.value;
+
+        // Display news articles
+        const newsResultsDiv = document.querySelector('.news-search-results');
+        newsResultsDiv.innerHTML = '';
+        newsArticles.forEach(article => {
+          const articleTitle = article.name;
+          const articleUrl = article.url;
+          const articleDescription = article.description;
+                // Create and append the news article element
+      const newsItemDiv = document.createElement('div');
+      newsItemDiv.classList.add('news-result-item');
+      newsItemDiv.innerHTML = `
+        <h3><a href="${articleUrl}" target="_blank">${articleTitle}</a></h3>
+        <p>${articleDescription}</p>
+      `;
+      newsResultsDiv.appendChild(newsItemDiv);
+    });
   })
-    .then(response => response.json())
-    .then(data => {
-      // Get the news articles
-      const newsArticles = data.value;
-      
-      // Display news articles
-      const newsListDiv = document.querySelector('.news-list');
-      newsArticles.forEach(article => {
-        const articleTitle = article.name;
-        const articleUrl = article.url;
-        const articleDescription = article.description;
-        
-    // Create and append the news article element
-    const newsItemDiv = document.createElement('div');
-    newsItemDiv.classList.add('news-item');
-    newsItemDiv.innerHTML = `
-      <h3><a href="${articleUrl}" target="_blank">${articleTitle}</a></h3>
-      <p>${articleDescription}</p>
-    `;
-    newsListDiv.appendChild(newsItemDiv);
+  .catch(error => {
+    console.error('Error fetching news search data:', error);
   });
-})
-.catch(error => {
-  console.error('Error fetching anime news data:', error);
-});
 }
 
 // Add event listener to the search button on the search page
 const searchButton = document.querySelector('.search-button');
 if (searchButton) {
-searchButton.addEventListener('click', searchAnime);
-}
-
-// Fetch and display anime news on the news page when the news page is loaded
-if (document.querySelector('.news-list')) {
-fetchAnimeNews();
+searchButton.addEventListener('click', searchAnimeAndNews);
 }
